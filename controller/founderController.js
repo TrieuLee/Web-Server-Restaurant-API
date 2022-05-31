6eb188df43305c7138b5a5ca37603dc9b1cf556b
TrieuLee <lehaitrieu48@gmail.com>
2022-05-31 12:38:55 +0700

    git pull

--- controller/founderController.js
+++ controller/founderController.js
@@ -219,6 +219,7 @@ class FounderController {
       res
         .cookie("token", token, {
           httpOnly: true,
+          sameSite: "Strict",
         })
         .send();
       // tạo jsonwebtoken cho để lấy token cho user
