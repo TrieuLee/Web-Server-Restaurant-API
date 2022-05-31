cf7c7f06304a822b22a06c26adf3b9631a46704a
TrieuLee <lehaitrieu48@gmail.com>
2022-05-31 14:51:13 +0700

    change restaurant API

--- routers/restaurantRouter.js
+++ routers/restaurantRouter.js
@@ -3,14 +3,18 @@ const auth = require("../middleware/auth");
 
 const RestaurantController = require("../controller/restaurantController");
 
-router.get("/", auth, RestaurantController.getRestaurantList);
+router.get("/:token", auth, RestaurantController.getRestaurantList);
 
-router.get("/byFounder", auth, RestaurantController.getRestaurantByFounder);
+router.get(
+  "/byFounder/:token",
+  auth,
+  RestaurantController.getRestaurantByFounder
+);
 
-router.post("/", auth, RestaurantController.createRestaurants);
+router.post("/:token", auth, RestaurantController.createRestaurants);
 
-router.put("/:id", auth, RestaurantController.updateRestaurant);
+router.put("/:id/:token", auth, RestaurantController.updateRestaurant);
 
-router.delete("/:id", auth, RestaurantController.deleteRestaurant);
+router.delete("/:id/:token", auth, RestaurantController.deleteRestaurant);
 
 module.exports = router;
