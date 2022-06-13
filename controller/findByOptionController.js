7d837a864e60ddc3a6e078c144da4b02deebed35
TrieuLee <lehaitrieu48@gmail.com>
2022-06-14 00:23:35 +0700

    .

--- controller/findByOptionController.js
+++ controller/findByOptionController.js
@@ -6,24 +6,29 @@ class FindByOption {
   async findRestaurant(req, res) {
     try {
       const { keyfind } = req.body;
-      const { count, rows } = await Restaurant.findAndCountAll({
-        where: {
-          [Op.or]: [
-            {
-              Res_name: {
-                [Op.like]: `%${keyfind}%`,
+      if (keyfind === "") {
+        const res = await Restaurant.findAll();
+        res.json(res);
+      } else {
+        const { count, rows } = await Restaurant.findAndCountAll({
+          where: {
+            [Op.or]: [
+              {
+                Res_name: {
+                  [Op.like]: `%${keyfind}%`,
+                },
               },
-            },
-            {
-              Res_address: {
-                [Op.like]: `%${keyfind}%`,
+              {
+                Res_address: {
+                  [Op.like]: `%${keyfind}%`,
+                },
               },
-            },
-          ],
-        },
-        limit: 10,
-      });
-      res.json({ count, rows });
+            ],
+          },
+          limit: 10,
+        });
+        res.json({ count, rows });
+      }
     } catch (error) {
       res.status(500).send();
     }
