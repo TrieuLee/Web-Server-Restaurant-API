8b6b343014559d8acca87dc68092492d38e6ef94
TrieuLee <lehaitrieu48@gmail.com>
2022-06-14 00:00:50 +0700

    change typeof pic food

--- models/food.js
+++ models/food.js
@@ -29,7 +29,7 @@ module.exports = (sequelize) => {
       allowNull: false,
     },
     Fd_image: {
-      type: DataTypes.BOOLEAN,
+      type: DataTypes.STRING,
       allowNull: false,
     },
   });
